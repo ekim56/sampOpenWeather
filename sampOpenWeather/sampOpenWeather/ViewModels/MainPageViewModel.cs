de14ca38cb5d2e4fd5a354433f4f56c95ce8c611
Michael <Michael@DESKTOP-C35THM2>
2022-06-17 18:10:11 +0800

    Add project files.

---
+++ sampOpenWeather/sampOpenWeather/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+using Newtonsoft.Json;
+using System.Net;
+using sampOpenWeather.Models;
+using System.Windows.Input;
+
+
+namespace sampOpenWeather.ViewModels
+{
+    public class MainPageViewModel : BindableObject
+    {
+       
+     
+       
+     
+      
+
+    }
+
+
+}
+ 
+   
+  
+
---
+++ sampOpenWeather/sampOpenWeather/Views/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Newtonsoft.Json;
+using System.Net;
+using sampOpenWeather.Models;
+
+namespace sampOpenWeather
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            getOpenWeather();
+        }
+        string APIkey = "07795c1926651e85a707110092641b54";
+        void getOpenWeather()
+        {
+            using (WebClient web = new WebClient())
+            {
+                string url = string.Format("http://api.openweathermap.org/data/2.5/weather?q=Philippines,Manila&APPID=07795c1926651e85a707110092641b54", APIkey);
+                var json = web.DownloadString(url);
+                OpenWeatherInfoModel.alldata data = JsonConvert.DeserializeObject<OpenWeatherInfoModel.alldata>(json);
+                weather.Text = data.weather[0].main;
+                description.Text = data.weather[0].description;
+                temp.Text = data.main.temp.ToString();
+                Sunset.Text = data.sys.sunset.ToString();
+                Sunrise.Text = data.sys.sunrise.ToString();
+                windspeed.Text = data.wind.speed.ToString();
+                pressure.Text = data.main.pressure.ToString();
+
+
+            }
+
+        }
+    }
+}
